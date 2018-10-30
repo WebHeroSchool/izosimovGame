6b978d3a74f6a1b614e5ef7bf2377635118c11aa
Артём Изосимов <1996loko@rambler.ru>
2018-10-31 00:37:02 +0300

    used data attributes

--- scripts.js
+++ scripts.js
@@ -4,8 +4,23 @@ class CathMouse {
         this.lives = 3;
         this.level = 1;
         this.isRunning = false;
-        this.emojiTypes = ['lion', 'panda', 'bear', 'fox', 'cat', 'cow', 'pig', 'koala', 'rabbit', 'tiger'];
+
+        this.emojies = {
+            lion: '🦁',
+            panda: '🐼',
+            bear: '🐻',
+            fox: '🦊',
+            cat: '🐱',
+            cow: '🐮',
+            pig: '🐽',
+            koala: '🐨',
+            rabbit: '🐰',
+            tiger: '🐯',
+            mouse: '🐭'
+        };
+
         this.rightEmoji = 'mouse';
+
         this.holes = document.getElementsByClassName('playing-zone__hole');
         this.existingEmojies = document.getElementsByClassName('appearance-animal');
 
@@ -14,50 +29,55 @@ class CathMouse {
     startGame() {
         this.isRunning = true;
         this.createEmoji();
-        this.setIntervalId = setInterval(this.createEmoji, 2000);
+        this.intervalId = setInterval(this.createEmoji, 2000);
     }
     createEmoji() {
+        if (!this.isRunning) return;
+
+        // Этот while нужен, чтобы "чистить" грядки от эмоджи, по которым мы не кликали
         while (this.existingEmojies.length) {
+            console.log('kekes');
             this.existingEmojies[0].remove();
         };
-        if (this.isRunning) {
-            let emojiType = '';
-            if (Math.floor(Math.random() * 3) === 2) {
-                emojiType = this.rightEmoji;
-            } else {
-                let randomEmojiIndex = Math.floor(Math.random() * (this.emojiTypes.length));
-                emojiType = this.emojiTypes[randomEmojiIndex];
-            }
-            let randomHoleIndex = Math.floor(Math.random() * (this.holes.length));
 
-            let emoji = document.createElement('div');
-            emoji.classList.add(`playing-zone__${emojiType}`, 'appearance-animal');
-            this.holes[randomHoleIndex].appendChild(emoji);
-            emoji.addEventListener('click', this.emojiClickHandler.bind(this, emoji));
+        let emojiType = '';
+        const allEmojies = Object.keys(this.emojies);
+
+        if (Math.floor(Math.random() * 3) === 2) {
+            emojiType = this.rightEmoji;
+        } else {
+            const randomEmojiIndex = Math.floor(Math.random() * allEmojies.length);
+            emojiType = allEmojies[randomEmojiIndex];
         }
+        const randomHoleIndex = Math.floor(Math.random() * this.holes.length);
+
+        const emoji = document.createElement('div');
+        emoji.classList.add('appearance-animal');
+        emoji.dataset.emoji = this.emojies[emojiType];
+        this.holes[randomHoleIndex].appendChild(emoji);
+        emoji.addEventListener('click', this.emojiClickHandler.bind(this, emoji));
     }
     emojiClickHandler(emoji) {
-        if (emoji.classList.contains('playing-zone__mouse')) {
+        if (emoji.dataset.emoji === this.emojies[this.rightEmoji]) {
             console.log('Я поймал мышь');
-        }
+        };
         console.log(emoji);
+
+        clearInterval(this.intervalId);
         emoji.remove();
-        this.clearIntervals();
+
         this.createEmoji();
-        this.setIntervalId = setInterval(this.createEmoji, 2000);
+        this.intervalId = setInterval(this.createEmoji, 2000);
     }
     endGame() {
         this.isRunning = false;
-        this.clearIntervals();
-    }
-    clearIntervals() {
-        clearInterval(this.setIntervalId);
+        clearInterval(this.intervalId);
     }
 }
 
-let newGame = new CathMouse();
+const newGame = new CathMouse();
 
-let btn = document.querySelector('.start-bar__start-btn');
+const btn = document.querySelector('.start-bar__start-btn');
 btn.addEventListener('click', () => {
     newGame.startGame();
 });
